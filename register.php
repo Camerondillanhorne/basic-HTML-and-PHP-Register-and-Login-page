f54df74223d4862152f0f241c0fff3ea82cadc93
Camerondillanhorne <125814966+Camerondillanhorne@users.noreply.github.com>
2023-02-21 14:05:02 -0500

    Update register.php

--- register.php
+++ register.php
@@ -1,16 +1,41 @@
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-	// get form data
-	$username = $_POST["username"];
-	$password = $_POST["password"];
+// database connection information
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "users";
 
-	// store username and password in a file
-	$file = fopen("users.txt", "a");
-	fwrite($file, "$username|$password\n");
-	fclose($file);
+// create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-	// redirect to login page
-	header("Location: login.html");
-	exit;
+// check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+// if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // check if the username already exists in the database
+    $sql = "SELECT * FROM user_details WHERE username='$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "Username already exists. Please choose a different username.";
+    } else {
+        // insert the new user details into the database
+        $sql = "INSERT INTO user_details (username, password) VALUES ('$username', '$password')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Registration successful!";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+}
+
+// close the database connection
+$conn->close();
 ?>
